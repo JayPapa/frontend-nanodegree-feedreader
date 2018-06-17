b7cc8a4a9dff85702a7c6cfcdec7473f34efc034
Jorgo Papa <jp.jp13p@gmail.com>
2018-06-17 19:45:54 +0200

    Add 'menu changes visibility when clicked' spec

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -38,7 +38,7 @@ $(function() {
             };
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -53,18 +53,31 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
+        /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
 
-         /* TODO: Write a test that ensures the menu changes
+         it('menu is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
+
+         /* Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+          it('menu changes visibility when clicked', function(){
+            $('.menu-icon-link').click()
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click()
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
